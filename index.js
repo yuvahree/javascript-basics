5c9818aa8b5e49cf1b8560c4dc061939940bc576
yuvahree <107060025+yuvahree@users.noreply.github.com>
2022-07-09 11:03:51 +0530

    learning of basics in js

---
+++ index.js
@@ -0,0 +1,142 @@
+//alert("hello sir");
+  
+//let
+
+let name="yuva";
+let msg="Hello my name is" + name +"im learning js";
+alert(msg);
+console.log(msg);
+
+let number;
+console.log(number);
+
+let number1=5;
+let number2=20;
+
+let result=number1+number2;
+console.log(result);
+
+let char ="aaa";
+  char ="bbb";
+console.log(char);
+
+/*let if=5;  //syntax error
+console.log(result);*/ 
+
+let number5;
+console.log(result); 
+
+let x = 5; 
+console.log(x);
+
+let a = 'hello';
+let _a = 'hello';
+let $a = 'hello';
+
+let z = "hi";
+let Y = 5;
+
+console.log(z); // hi
+console.log(Y); // 5
+
+let sum = 44;
+console.log(sum);   // 44
+
+
+
+//const
+
+const y = 90;
+console.log(y);
+
+
+const greet = 'Hello';
+const greeting = 'Jack';
+
+console.log(greet + ' ' + greeting);
+
+  /*const x = 5;
+x = 10;  // Error! constant cannot be changed.
+console.log(x)*/
+
+//datatypes
+
+//string
+const name2 = 'ram';
+const name1 = "hari";
+const result1 = `The names are ${name} and ${name1}`;
+
+let length = 16;                               // Number
+let lastName = "Johnson";                      // String
+let k = {firstName:"John", lastName:"Doe"}; 
+
+let strg = 'JavaScript';
+strg = strg + ' String';
+
+let str = "Hi";
+console.log(typeof(str));
+
+let firstname = 'John';
+let greeti = `Hi, I'm ${firstname}.`;
+console.log(greeti);
+
+let strin = "Good Morning!";
+console.log(strin.length);  
+
+let counter = 120;
+console.log(typeof(counter)); 
+
+let num = 071;
+console.log(num);
+
+let amount = 3.14e7;
+console.log(amount);
+
+let amt = 0.2 + 0.1;
+console.log(amt);
+
+let message = false; 
+console.log(typeof(message)); 
+
+let count;
+console.log(count);        // undefined
+console.log(typeof count); 
+
+let obj = null;
+console.log(typeof obj); 
+
+let inProgress = true;
+let completed = false;
+console.log(typeof completed);
+
+let resul = 'a' < 'b';
+console.log(resul); 
+
+let isPending = false;
+let isDone = true;
+console.log(typeof(isPending));
+console.log(typeof(isDone)); 
+
+let e = Boolean('Hi');
+console.log(a); 
+console.log(typeof(a)); 
+
+const student = {firstName: 'ram',lastName: null,class: 10};
+console.log(student); 
+
+//array
+
+let person = {firstName: 'John',lastName: 'Doe'};
+console.log(person['firstName']);
+console.log(person['lastName']);
+
+/*address['building no'];
+address.'building no'; //unexpected string */
+
+let list = {firstName: 'John',lastName: 'Doe'};
+list.firstName = 'Jane';
+console.log(list);
+
+let employee = {firstName: 'Peter',lastName: 'Doe',employeeId: 1};
+console.log('ssn' in employee);
+console.log('employeeId' in employee);
